cda5398dfc24c087fe5d069211793e504bc8fb6e
Moiz-Adeeb <moizadeebofficial@gmail.com>
2025-07-15 16:57:22 +0500

    certification edits

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,5 +1,5 @@
-import project1_img from '../assets/certificate-1.png'
-import project2_img from '../assets/certificate-2.png'
+import project1_img from '../assets/IBM Back-end JavaScript.png'
+import project2_img from '../assets/IBM Front-End Developer.png'
 import project3_img from '../assets/certificate-3.png'
 import project4_img from '../assets/certificate-4.png'
 import project5_img from '../assets/certificate-5.png'
@@ -9,33 +9,35 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        w_link:"https://www.coursera.org/account/accomplishments/professional-cert/9FHN442OBTGI"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        w_link:"https://www.coursera.org/account/accomplishments/professional-cert/SEGHR4SNKCXT"
     },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
+    // {
+    //     w_no:3,
+    //     w_name:"Web design",
+    //     w_img:project3_img
+    // },
+    // {
+    //     w_no:4,
+    //     w_name:"Web design",
+    //     w_img:project4_img
+    // },
+    // {
+    //     w_no:5,
+    //     w_name:"Web design",
+    //     w_img:project5_img
+    // },
+    // {
+    //     w_no:6,
+    //     w_name:"Web design",
+    //     w_img:project6_img
+    // },
 ]
  
 export default mywork_data;
